5cd5e488bf081eec24294a37747b066b484ae933
asturias-leyenda <schneidermalte49@gmail.com>
2021-02-24 21:22:00 +0100

    improved code readability

--- endpoints/text/routes.py
+++ endpoints/text/routes.py
@@ -3,16 +3,19 @@ from flask import Blueprint
 from endpoints.text.functions import *
 from transformers import pipeline
 summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", framework="tf")
-
+import json
 
 text = Blueprint('text', __name__)
 
 @text.route('/summarize', methods=['POST', 'GET'])
 def summarize():
-    if request.content_type == 'text/plain':
-        print('text/plain')
-        text_plain = request.data
-    if request.content_type == 'application/json' and request.accept_mimetypes['application/json']==1:
+    json = 'application/json'
+    text = 'text/plain'
+    accept_json = request.accept_mimetypes['application/json']
+    accept_text = request.accept_mimetypes['application/json']
+
+
+    if request.content_type == json and accept_json==1:
         print('application/json')
         if request.is_json:
             text = request.json.get("text")
@@ -20,17 +23,12 @@ def summarize():
             summary_content = summarizer(str(text), min_length=5, max_length=10)
             summary = str(summary_content)
 
-            response = Response(summary, mimetype='application/json')
-
-            return response
-            
-    if request.content_type == 'application/json' and request.accept_mimetypes['application/json']==1:
-        print('application/json')
-        if request.is_json:
-            text = request.json.get("text")
-            aim = request.json.get("aim")
-            summary_content = summarizer(str(text), min_length=5, max_length=10)
-            summary = str(summary_content)
+            # response = app.response_class(
+            #     response=json.dumps(summary),
+            #     status=200,
+            #     mimetype='application/json'
+            # ) 
+            # -> not working out of the box -> customizing of the flask response class necessary
 
             response = Response(summary, mimetype='application/json')
 
