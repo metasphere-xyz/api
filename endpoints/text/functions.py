4b542c8f0469259593d8eaa04e9c3b26a46d0b9a
asturias-leyenda <schneidermalte49@gmail.com>
2021-02-25 15:31:04 +0100

    current status

--- endpoints/text/functions.py
+++ endpoints/text/functions.py
@@ -4,35 +4,61 @@ summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", f
 def summary(text_input, aim=50, deviation_input=10, num_summaries=1):
     # text_input = "This is an input text for the summarizer"
 
-    if num_summaries > 1:
-        min_length = [0] * num_summaries
-        max_length = [0] * num_summaries
-
-        for i in range(num_summaries):
-            min_length[i] = aim + i*deviation_input
-            max_length[i] = aim + (i+1)*deviation_input
-
-    text_input_length = len(list(text_input.split()))
     aim_rel = aim/100
 
-    min_length_rel = aim - deviation_input
-    max_length_rel = aim + deviation_input
+    if num_summaries > 1:
+        min_length_rel = [0] * num_summaries
+        max_length_rel = [0] * num_summaries
+        min_length_words = [0] * num_summaries
+        max_length_words = [0] * num_summaries
+        summary_output = [""] * num_summaries
+        text_input_length = len(list(text_input.split()))
+        aim_mult = [0] * num_summaries
+        compression = [0] * num_summaries
 
-    min_length_abs = round(text_input_length * (min_length_rel/100))
-    max_length_abs = round(text_input_length * (max_length_rel/100))
-    
-    summary_output = summarizer(
-        text_input, 
-        min_length=min_length_abs, 
-        max_length=max_length_abs
-    )
-    text = str(summary_output)
-    text_length = len(list(text.split()))
-    compression = text_length / text_input_length
-    deviation_output = abs(compression - aim_rel)
+        for i in range(num_summaries):
+            min_length_rel[i] = (aim + i*deviation_input)/100
+            max_length_rel[i] = (aim + (i+1)*deviation_input)/100
+            min_length_words[i] = round(text_input_length * (aim + i*deviation_input)/100)
+            max_length_words[i] = round(text_input_length * (aim + (i+1)*deviation_input)/100)
+            aim_mult[i] = min_length_rel[i] + (deviation_input/2)/100
 
-    print("compression:", compression)
-    print("aim", aim_rel)
-    print("deviation:", deviation_output)
+        for i in range(num_summaries):
+            summary_output[i] = summarizer(
+                text_input, 
+                min_length=min_length_words[i], 
+                max_length=max_length_words[i]
+            )
+            text = str(summary_output)
+            text_length = len(list(text.split()))
+
+            compression = text_length / text_input_length
+            deviation_output = abs(compression - aim_rel)
+
+        # print("aim_mult)
+        # print(min_length_rel)
+
+    if num_summaries == 1:
+        text_input_length = len(list(text_input.split()))
+
+        min_length_rel = aim - deviation_input
+        max_length_rel = aim + deviation_input
+
+        min_length_abs = round(text_input_length * (min_length_rel/100))
+        max_length_abs = round(text_input_length * (max_length_rel/100))
+        
+        summary_output = summarizer(
+            text_input, 
+            min_length=min_length_abs, 
+            max_length=max_length_abs
+        )
+        text = str(summary_output)
+        text_length = len(list(text.split()))
+        compression = text_length / text_input_length
+        deviation_output = abs(compression - aim_rel)
+
+    # print("compression:", compression)
+    # print("aim", aim_rel)
+    # print("deviation:", deviation_output)
 
     return  text
