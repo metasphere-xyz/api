7520b6b353b7f90187489c631d3c71b00a0e2670
asturias-leyenda <schneidermalte49@gmail.com>
2021-02-26 16:02:44 +0100

    final summary json->json  endpoint

--- endpoints/text/routes.py
+++ endpoints/text/routes.py
@@ -3,40 +3,21 @@ from flask import Blueprint
 from endpoints.text.functions import *
 import json
 
-# from transformers import pipeline
-# summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", framework="tf")
-# import json
-
 text = Blueprint('text', __name__)
 
 @text.route('/summarize', methods=['POST', 'GET'])
 def summarize():
+    # Input Parameter: Summary
+    aim = 20
+    deviation = 15
+    num_summaries = 3
+
     app_json = 'application/json'
     text = 'text/plain'
     html = 'text/html'
     accept_json = request.accept_mimetypes['application/json']
     accept_text = request.accept_mimetypes['text/plain']
     accept_html = request.accept_mimetypes['text/html']
-
-    # # HTML -> MTML
-    # if request.content_type == html and accept_html==1:
-        
-    #     response = Response(summary, mimetype=html)
-
-    # # TEXT/PLAIN -> TEXT/PLAIN
-    # if request.content_type == text and accept_text==1:
-        
-    #     response = Response(summary, mimetype=text)
-    
-    # # TEXT/PLAIN -> APPLICATION/JSON
-    # if request.content_type == text and accept_text==1:
-        
-    #     response = Response(summary, mimetype=json) 
-    
-    # # JSON -> TEXT/PLAIN
-    # if request.content_type == text and accept_text==1:
-        
-    #     response = Response(summary, mimetype=text) 
 
     # JSON -> JSON
     if request.content_type == app_json and accept_json==1:
@@ -63,34 +44,44 @@ def summarize():
                     }
                 )
                 text = req_json["chunk_sequence"][i]["text"]
-                text_chunk[i], compression[i], aim_rel[i], deviation_output[i] = summary(text, aim=20, num_summaries=1, chunk_number=chunk_number)
+                text_chunk[i], compression[i], aim_rel[i], deviation_output[i] = summary(text, aim=aim, deviation_input=deviation, num_summaries=num_summaries, chunk_number=chunk_number)
 
-                data["chunk_sequence"][i]["summary"].append(
-                    {
-                        "text": text_chunk[i],
-                        "summary_id": "md5sum",
-                        "compression": compression[i],
-                        "aim": aim_rel[i],
-                        "deviation": deviation_output[i]
-                    },
+                for j in range(num_summaries):
+                    data["chunk_sequence"][i]["summary"].append(
+                        {
+                            "text": text_chunk[i][j],
+                            "summary_id": j,
+                            "compression": compression[i][j],
+                            "aim": aim_rel[i][j],
+                            "deviation": deviation_output[i][j]
+                        },
             )
 
             data_json = json.dumps(data)
-            print(data_json)
 
-            # data = {
-            #     "text": text_chunk,
-            #     "compression": compression,
-            #     "aim_rel": aim_rel,
-            #     "deviation_output": deviation_output
-            #     }
+            response = make_response(data_json)
+            response.mimetype=app_json
 
-            # response = make_response(data)
-            # response.mimetype=json
-
-            # return response
-            return "check"
+            return response
     
-    
+    # # HTML -> MTML
+    # if request.content_type == html and accept_html==1:
+        
+    #     response = Response(summary, mimetype=html)
 
-    return "test"
+    # # TEXT/PLAIN -> TEXT/PLAIN
+    # if request.content_type == text and accept_text==1:
+        
+    #     response = Response(summary, mimetype=text)
+    
+    # # TEXT/PLAIN -> APPLICATION/JSON
+    # if request.content_type == text and accept_text==1:
+        
+    #     response = Response(summary, mimetype=json) 
+    
+    # # JSON -> TEXT/PLAIN
+    # if request.content_type == text and accept_text==1:
+        
+    #     response = Response(summary, mimetype=text) 
+    
+    return "endpoint: summary"
