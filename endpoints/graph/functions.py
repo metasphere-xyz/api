7aaca405c0fdbc2c7355b13caeae99f9a59fb992
asturias-leyenda <schneidermalte49@gmail.com>
2021-03-03 14:48:35 +0100

    valid json response

--- endpoints/graph/functions.py
+++ endpoints/graph/functions.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from py2neo import Graph
 graph = Graph(
     # "bolt://127.0.0.1:7687",
@@ -5,14 +6,23 @@ graph = Graph(
     auth=('neo4j', 'burr-query-duel-cherry')
 )
 
+response = {
+        "status": "success",
+        "node": {
+
+        }
+    }
+
 # graph functions / cypher commands
 def find_node(node_id):
     query = '''
         MATCH (c:Chunk {chunk_id: $chunk_id})
-        RETURN c
+        RETURN c as chunk
     '''
     result = graph.run(query, parameters={'chunk_id': node_id}).data()
-    return result
+    result = result[0]['chunk']
+    response['node']=result
+    return response
 
 # TODO: add missing functions for other endpoints
     
