84dbc73f174d72796dea211028537db2b068ba64
Julian Fleck <mail@julianfleck.net>
2021-03-10 19:08:42 +0100

    added function parse_json(endpoint)

--- endpoints/text/request.py
+++ endpoints/text/request.py
@@ -19,3 +19,33 @@ def parse_json_similarity():
         return text, num_chunks
     else:
         return 'json expected'
+
+def parse_json(endpoint):
+    if request_type(request) != 'application/json':
+        return 'json expected'
+    else:
+        if endpoint == 'similarity':
+            text = request.get_json()['text']
+            num_chunks = request.get_json()['chunks']
+            # define standard values
+            if not num_chunks:
+                num_chunks = 3
+            return text, num_chunks
+        elif endpoint == 'text':
+            text = request.get_json()['text']
+            return text
+        elif endpoint == 'summarize':
+            text = request.get_json()["text"]
+            aim = request.get_json()["aim"]
+            deviation = request.get_json()["deviation"]
+            num_summaries = request.get_json()["num_summaries"]
+            # define standard values
+            if not text:
+                raise_error("no text specified")
+            if not aim:
+                aim = "50"
+            if not deviation:
+                deviation = "10"
+            if not num_summaries:
+                num_summaries = "1"
+            return text, aim, deviation, num_summaries
--- endpoints/text/routes.py
+++ endpoints/text/routes.py
@@ -3,50 +3,35 @@ from flask import Blueprint
 import json
 
 # Custom functions
+from config import *
 from functions import *
 from endpoints.text.functions import *
 from endpoints.text.text_processing import *
 from endpoints.text.request import *
 from endpoints.text.response import *
 
-
 text = Blueprint('text', __name__)
 
 @text.route('/extract/entities', methods=['POST', 'GET'])
 def return_ner():
-    text = get_text_json()
+    text = parse_json('text')
     ner_output = ner(text)
     response = response_is_json(ner_output)
     return response
 
 @text.route('/similarity', methods=['POST', 'GET'])
 def return_similarities():
-    text, num_similar_chunks = parse_json_similarity()
+    text, num_similar_chunks = parse_json('similarity')
     similar_chunks = similarity_tf(text, num_similar_chunks)
     response = response_is_json(similar_chunks)
     return response
 
 @text.route('/summarize', methods=['POST', 'GET'])
 def return_summaries():
-    # Parse input parameters from request
     if request_type(request) == 'text/plain':
         text = request
     elif request_type(request) == 'application/json':
-        # parse request values from JSON
-        text = request.get_json()["text"]
-        aim = request.get_json()["aim"]
-        deviation = request.get_json()["deviation"]
-        num_summaries = request.get_json()["num_summaries"]
-
-    # define standard values
-    if not text:
-        raise_error("no text specified")
-    if not aim:
-        aim = "50"
-    if not deviation:
-        deviation = "10"
-    if not num_summaries:
-        num_summaries = "1"
+        (text, aim, deviation, num_summaries) = parse_json('summarize')
 
     # calculate response via summarize function
     summary = summarize(
@@ -77,84 +62,3 @@ def return_summaries():
         except Exception as ex:
             traceback.print_exc()
             return {'status': 'failed', 'error': str(ex)}
-
-# TODO: add missing endpoints:
-# /text/extract/chunks
-# /text/extract/entities
-# /text/find/similar -- working title
-
-    #
-    #
-    # if request.content_type == app_json and accept_json==1:
-    #
-    #     data = {
-    #             "chunk_id": "md5sum",
-    #             "summary": [
-    #             ]
-    #         }
-    #
-    #     if request.is_json:
-    #         req_json = request.get_json()
-    #
-    #         text = req_json["text"]
-    #         aim = req_json["aim"]
-    #         deviation = req_json["deviation"]
-    #         num_summaries = req_json["num_summaries"]
-    #
-    #         text_chunk = [""] * num_summaries
-    #         compression = [0] * num_summaries
-    #         aim_rel = [0] * num_summaries
-    #         deviation_output = [0] * num_summaries
-    #
-    #         # Generate n>1 summaries by multiple summary call -> no num_summaries case distinction
-    #         text_chunk, compression, aim_rel, deviation_output = summary(text, aim=aim, deviation_input=deviation, num_summaries=num_summaries)
-    #
-    #         if num_summaries>1:
-    #             for i in range(num_summaries):
-    #                 data["summary"].append(
-    #                     {
-    #                         "text": text_chunk[i],
-    #                         "summary_id": i,
-    #                         "compression": compression[i],
-    #                         "aim": aim_rel[i],
-    #                         "deviation": deviation_output[i]
-    #                     },
-    #                 )
-    #         else:
-    #             data["summary"].append(
-    #                     {
-    #                         "text": text_chunk,
-    #                         "summary_id": 1,
-    #                         "compression": compression,
-    #                         "aim": aim_rel,
-    #                         "deviation": deviation_output
-    #                     },
-    #                 )
-    #
-    #         data_json = json.dumps(data)
-    #         response = make_response(data_json)
-    #         response.mimetype=app_json
-    #
-    #         return response
-    #
-    # # # HTML -> MTML
-    # # if request.content_type == html and accept_html==1:
-    #
-    # #     response = Response(summary, mimetype=html)
-    #
-    # # # TEXT/PLAIN -> TEXT/PLAIN
-    # # if request.content_type == text and accept_text==1:
-    #
-    # #     response = Response(summary, mimetype=text)
-    #
-    # # # TEXT/PLAIN -> APPLICATION/JSON
-    # # if request.content_type == text and accept_text==1:
-    #
-    # #     response = Response(summary, mimetype=json)
-    #
-    # # # JSON -> TEXT/PLAIN
-    # # if request.content_type == text and accept_text==1:
-    #
-    # #     response = Response(summary, mimetype=text)
-    #
-    # return "endpoint: summary"
